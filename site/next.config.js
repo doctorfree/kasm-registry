7ff2248ceeeb713ad3aba22fb1ee88cd43040517
Ronald Record <github@ronrecord.com>
2025-06-25 14:18:32 -0700

    Use default basePath

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/doctorfree',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
